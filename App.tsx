02fa6f25b2bc95eb63fb473efb30449625c6394c
Jeff Dorsainvil <joedor7@gmail.com>
2023-04-10 14:48:51 -0400

    added theme.

--- App.tsx
+++ App.tsx
@@ -17,6 +17,5 @@ const styles = StyleSheet.create({
   },
   text: {
     padding: 1,
-
   }
 })
---
+++ theme/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  PRIMARY: '#4E3CF1',
+  SECONDARY_ORANGE: '#EC753A',
+  GRAY_BG: '#121213',
+  GRAY_ACCENT: '#1E1E1E',
+  WHITE_BG: '#FFF',
+}
